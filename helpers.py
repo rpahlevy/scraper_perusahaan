ecbd7b2d836bf485ab40d6a11583979e3dc4f31d
rpahlevy <43176811+rpahlevy@users.noreply.github.com>
2020-05-28 15:50:08 +0700

    add helper and new spider manufakturindo

---
+++ helpers.py
@@ -0,0 +1,85 @@
+import re
+
+
+def fix_title(title):
+    # remove non utf
+    title = bytes(title, 'utf-8').decode('utf-8', 'ignore')
+    # remove non alpha and some characters
+    title = re.sub('[^a-zA-Z0-9 \-,\.]', '', title)
+    title = title.strip()
+    title = title.strip(' ,.')
+    slug = title.lower()
+    # helper
+    last2 = slug[-2:]
+    # remove pt
+    if last2 == 'pt':
+        title = 'PT ' + title[:-2]
+    # remove cv
+    if last2 == 'cv':
+        title = 'CV ' + title[:-2]
+    # strip
+    title = title.strip(' ,.')
+    return title
+
+def get_slug(title, replace_space=''):
+    slug = title.lower().strip()
+    slug = slug.strip(' ,.')
+    # split by '-'
+    if '-' in slug:
+        slug = slug.split('-')
+        if len(slug[-1].split(' ')) > 3:
+            slug[-1] = slug[-1].strip()
+            slug = ''.join(slug)
+        else:
+            slug = ''.join(slug[:-1])
+        slug = slug.strip(' ,.')
+    # split by '|'
+    if '|' in slug:
+        slug = slug.split('|')[0]
+        slug = slug.strip(' ,.')
+    # split by ':'
+    if ':' in slug:
+        slug = slug.split(':')[0]
+        slug = slug.strip(' ,.')
+    # remove ()
+    slug = re.sub('[\(\)\/\/]', '', slug)
+    slug = slug.strip(' ,.')
+    # skip helper if len <= 4
+    if len(slug) <= 4:
+        if len(replace_space) > 0:
+            slug = re.sub(' ', replace_space, slug).strip()
+        return slug
+    # helper
+    first2 = slug[:2]
+    first3 = slug[:3]
+    last2 = slug[-2:]
+    last3 = slug[-3:]
+    last4 = slug[-4:]
+    # cek pt & pt.
+    if first2 == 'pt':
+        slug = slug[2:]
+    if last2 == 'pt':
+        slug = slug[:-2]
+    slug = slug.strip(' ,.')
+    # cek cv & cv.
+    if first2 == 'cv':
+        slug = slug[2:]
+    if last2 == 'cv':
+        slug = slug[:-2]
+    slug = slug.strip(' ,.')
+    # cek tbk & tbk.
+    if last3 == 'tbk':
+        slug = slug[:-3]
+    slug = slug.strip(' ,.')
+    # cek ltd & ltd.
+    if last3 == 'ltd':
+        slug = slug[:-3]
+    slug = slug.strip(' ,.')
+    # cek co & co.
+    if last2 == 'co':
+        slug = slug[:-2]
+    slug = slug.strip(' ,.')
+    # replace_space
+    if len(replace_space) > 0:
+        slug = re.sub(' ', replace_space, slug).strip()
+    return slug
---
+++ scraper_perusahaan/spiders/manufakturindo.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import helpers
+
+
+class ManufakturindoSpider(scrapy.Spider):
+    name = 'manufakturindo'
+    allowed_domains = ['manufakturindo.com']
+    start_urls = ['https://manufakturindo.com/company/']
+
+    def parse(self, response):
+        # get list perusahaan
+        for node in response.css('.list-compy'):
+            node_url = node.css('.main-list-compy > .list-content > a::attr(href)').get()
+            yield response.follow(node_url, callback=self.parse_detail)
+        # get next page url
+        next_url = response.css('.pagination-next > a::attr(href)').get()
+        if next_url is not None:
+            yield response.follow(next_url, callback=self.parse)
+
+    def parse_detail(self, response):
+        category = ''
+        name = ''
+        address = ''
+        city = ''
+        phone = ''
+        fax = ''
+        email = ''
+        website = ''
+        description = ''
+        url = response.url or ''
+        image_url = ''
+        # check type
+        lis = response.css('.comp-body li')
+        trs = response.css('table.table.description tr')
+        if len(lis) > 0:
+            # type 1
+            for li in lis:
+                k = li.css('::text').get().strip().split(':')[0].strip()
+                v = li.css('::text').get().strip().split(':')[-1].strip()
+                if len(k) == 0 or len(v) == 0:
+                    continue
+                if 'Company Name' in k:
+                    name = v
+                elif 'Address' in k:
+                    address = v
+                elif 'Telephone' in k:
+                    phone = li.css('a::text').get().strip()
+                elif 'Fax' in k:
+                    fax = v
+                elif 'Email' in k:
+                    email = v
+            # description
+            description = []
+            for p in response.css('.comp-row > p::text'):
+                txt = p.get().strip()
+                if len(txt) == 0 or 'Description' in txt:
+                    continue
+                description.append(txt)
+            description = ' '.join(description)
+            # website
+            website = response.css('.comp-row > p > a::attr(href)').get() or ''
+            if self.name in website:
+                website = ''
+            # category
+            category = response.css('.title-comp .col-sm-10::text')[-1].get()
+            # image_url
+            image_url = response.css('.img-container img::attr(src)').get() or ''
+        elif len(trs) > 0:
+            # type 2
+            for tr in trs:
+                k = tr.css('td::text')[0].get()
+                v = tr.css('td::text')[-1].get()
+                if len(k) == 0 or len(v) == 0:
+                    continue
+                if 'Nama Perusahaan' in k:
+                    name = v
+                elif 'Alamat' in k:
+                    address = tr.css('td')[-1].css('p::text').get()
+                elif 'Kategori' in k:
+                    category = v
+                elif 'Telepon' in k:
+                    phone = tr.css('td')[-1].css('a::text').get()
+                elif 'Fax' in k:
+                    fax = tr.css('td')[-1].css('a::text').get()
+                elif 'Email' in k:
+                    email = tr.css('td')[-1].css('a::text').get()
+            # description
+            description = []
+            for p in response.css('.container > p::text'):
+                txt = p.get().strip()
+                if len(txt) == 0:
+                    continue
+                description.append(txt)
+            description = ' '.join(description)
+            # website
+            website = response.css('a.btn.btn-contactus.btn-go-to::attr(href)').get() or ''
+            if self.name in website:
+                website = ''
+            # image_url
+            image_url = response.css('img.center-img::attr(src)').get() or ''
+
+        if len(email) == 0:
+            self.logger.info('{} : EMPTY EMAIL'.format(url))
+        if len(phone) == 0:
+            self.logger.info('{} : EMPTY PHONE'.format(url))
+
+        if len(email) > 0 and len(phone) > 0:
+            name = helpers.fix_title(name)
+            slug = helpers.get_slug(name)
+            if image_url is not None and len(image_url) > 0:
+                image_url = image_url.strip()
+                ext = image_url.split('.')[-1]
+                image_name = slug
+                target_dir = 'images/{}.{}'.format(image_name, ext)
+                self.logger.info('downloading image: {} => {}'.format(image_url, target_dir))
+                urllib.request.urlretrieve(image_url, target_dir)
+            yield {
+                'category': category.strip(),
+                'name': name.strip(),
+                'slug': slug.strip(),
+                'address': address.strip(),
+                'city': city.strip(),
+                'phone': phone.strip(),
+                'email': email.strip(),
+                'website': website.strip(),
+                'description': description.strip(),
+                'url': url.strip(),
+            }
